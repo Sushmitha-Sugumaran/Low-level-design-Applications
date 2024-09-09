7fe92fbfe1858064f98d38ab27363e84b0abc761
Sushmitha-sugumaran <132361230+Sushmitha-Sugumaran@users.noreply.github.com>
2024-09-09 11:26:51 +0530

    Delete Movie Ticket Booking (BookMyShow ) Low Level Design Application directory

--- Movie Ticket Booking (BookMyShow ) Low Level Design Application/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BOOKMYSHOWManagament management = BOOKMYSHOWManagament.getmanagement();
-        management.initializeUser();
-        MoviesSection.initializeMovies();
-        EventsSection.initializeEvents();
-        management.receptionSection();
-    }
-}
